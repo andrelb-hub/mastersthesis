1a9b6272b952f4cb2324804d315e927278a5f385
André Luiz <andreluizmtg@gmail.com>
2023-11-07 22:21:47 -0300

    07/11

---
+++ C_C1_C2_Y.R
@@ -0,0 +1,11 @@
+alpha = 0.7
+P1 = 1
+P2 = 2
+m  = 100
+C1 = alpha * m / P1
+C2 = (1 - alpha) * m / P2
+C  = (C1^alpha) * (C2^(1 - alpha))
+P = (P1 * C1 + P2 * C2) / C
+Y = C1 + C2
+
+((1/2)^(1/2))^(-1)
